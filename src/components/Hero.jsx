e480885b099c13d4a76c524f5919ee167fc2674e
Kar2831 <karthikeyanm282006@gmail.com>
2026-08-03 14:16:15 +0530

    First commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import profile from '../assets/kevinRushProfile.png';
+import profile from '../assets/karthikeyanProfile.png';
 import {HERO_CONTENT} from '../constants/index.js';
 import { ImLinkedin } from "react-icons/im";
 import { FaGithub } from "react-icons/fa";
@@ -14,7 +14,7 @@ function Hero() {
             <p className='text-[1.3rem] lg:text-start sm:text-center w-full lg:text-4xl bg-gradient-to-r from-purple-500 via-violet-500 to-pink-600 bg-clip-text text-transparent animate-pulse'>ASPIRING FULLSTACK DEVELOPER</p>
             <p>{HERO_CONTENT}</p>
             <div className='flex flex-col space-y-10 mx-auto lg:flex-row space-x-20'>
-                <a download href="/Karthikeyan_M_Resume.pdf">
+                <a download href="/Karthikeyan-M-Resume-Infosys.pdf">
                 <button className='menu-button scale-100 hover:scale-110 hover:transition-all hover:duration-300'>Resume</button>
                 </a>
                 <a href="https://www.linkedin.com/in/karthikeyan-m-0a3a062a0/" target='_blank'>
